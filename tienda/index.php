7abe4095024f0b7c3ba4304d1072c503e7ced7cf
98Byron <105457485+98Byron@users.noreply.github.com>
2024-06-18 18:01:10 -0400

    creacion del carrito, agregando articulos y entregando el totao, se fijo la parte superior e inferior a todas las paginas y se agrego el "quienes somos"

--- tienda/index.php
+++ tienda/index.php
@@ -2,43 +2,10 @@
 include 'global/config.php';
 include 'global/conexion.php';
 include 'carrito.php';
+include 'cabecera.php';
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
 
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tienda</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
-</head>
-<body>
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-        <a class="navbar-brand"href="index.php">LOGO DE LA EMPRESA</a>
-        <button class="navbar-toggler" data-target="#my-nav" data-toggle="collapse" aria-controls="my-nav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div id="my-nav" class="collapse navbar-collapse">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                <a class="nav-link" href="#"> Ver carrito <span class="sr-only">(current)</span></a>  
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="index.php" tabindex="-1" aria-disabled="true">HOME</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">CARRITO(0)</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-    <br>
-    <br>
-    <div class="container" >
         <br>
         <div class="alert alert-success">
         <?php echo $mensaje;?>
@@ -68,6 +35,7 @@ include 'carrito.php';
                 data-toggle='popover'
                 data-trigger='hover'
                 data-content='<?php echo$producto['Descripcion'];?>'
+                height="317px"
                 >
 
                 
@@ -79,10 +47,10 @@ include 'carrito.php';
 
                 <form action="" method="post">
 
-                <input type ="text" name="id" id="id" value="<?php echo openssl_encrypt($producto['ID'],COD,KEY);?>">
-                <input type ="text" name="nombre" id="nombre" value="<?php echo openssl_encrypt($producto['Nombre'],COD,KEY);?>">
-                <input type ="text" name="precio" id="precio" value="<?php echo openssl_encrypt($producto['Precio'],COD,KEY);?>" >
-                <input type ="text" name="cantidad" id="cantidad" value="<?php echo openssl_encrypt(1,COD,KEY);?>">
+                <input type ="hidden" name="id" id="id" value="<?php echo openssl_encrypt($producto['ID'],COD,KEY);?>">
+                <input type ="hidden" name="nombre" id="nombre" value="<?php echo openssl_encrypt($producto['Nombre'],COD,KEY);?>">
+                <input type ="hidden" name="precio" id="precio" value="<?php echo openssl_encrypt($producto['Precio'],COD,KEY);?>" >
+                <input type ="hidden" name="cantidad" id="cantidad" value="<?php echo openssl_encrypt(1,COD,KEY);?>">
 
                 <<button class="btn btn-primary" 
                     name= "btnAccion" 
@@ -110,5 +78,6 @@ include 'carrito.php';
 })      
 </script>
 
-</body>
-</html>
+<?php
+include 'pie.php'
+?>
